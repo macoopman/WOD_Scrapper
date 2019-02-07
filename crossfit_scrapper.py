bf6641f419c117269c52a880fdead12da613853b
Mike Coopman <macoopman@gmail.com>
2019-02-06 16:38:59 -0800

    refactor and comments

--- crossfit_scrapper.py
+++ crossfit_scrapper.py
@@ -7,21 +7,27 @@ import requests
 import ssl
 
 
-url = "https://www.crossfit.com/"
-
-html = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-
-soup = BeautifulSoup(html.text, 'html.parser')
-
-
-workout = []
-count = 0
-for art in soup.find_all('article', {'class': "_2IQsj5-LQZkOE2YtIR0usu"}):
-    for p in art.find_all('p'):
-        workout.append(p.text.strip())
+def getWOD():
+    """
+    Make url request to crossfit.com and get the workout of the day
+    """
+    url = "https://www.crossfit.com/"
+    html = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(html.text, 'html.parser')
 
 
-workout_type = workout[0]
-workout_body = workout[1]
-print(workout_type)
-print(workout_body)
+    workout = []
+    # locate the proper html location of daily wod
+    for art in soup.find_all('article', {'class': "_2IQsj5-LQZkOE2YtIR0usu"}):
+        for p in art.find_all('p'):
+            workout.append(p.text.strip())
+    # return (workout_type, workout_contents)
+    return (workout[0], workout[1])
+
+
+
+
+if __name__ == "__main__":
+    wod = getWOD()
+    for line in wod:
+        print(line)
