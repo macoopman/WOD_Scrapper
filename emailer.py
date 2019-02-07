f814df2ee275b99b7de92d364d613691da04756b
Mike Coopman <macoopman@gmail.com>
2019-02-06 16:56:56 -0800

    start emailer

---
+++ emailer.py
@@ -0,0 +1,27 @@
+"""
+Set up email service for daily emailing
+"""
+
+import smtplib, ssl
+
+
+
+
+def emailer(sender_email, sender_password, reciever, message):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = sender_email  # Enter your address
+    receiver_email = reciever  # Enter receiver address
+    password = sender_password
+    message = message
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, reciever, message)
+
+
+if __name__ == "__main__":
+    message = "it works"
+
+    emailer("coopmanmike@gmail.com", , "macoopman@gmail.com", message)
